8132b826c82e00c13161e1a2642365adf814a58f
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2022-02-17 10:40:13 +0100

    Close #1

--- BinaryMath/src/main/java/binarymath/BinaryMath.java
+++ BinaryMath/src/main/java/binarymath/BinaryMath.java
@@ -1,35 +1,49 @@
 package binarymath;
 
+// 14 9
+
 public class BinaryMath {
     private int convertBinaryToDecimal(Integer number) {
         return Integer.parseInt(number.toString(), 10);
     }
 
-    public int binarySum(int binaryA, int binaryB){
+    public int binarySum(int binaryA, int binaryB) {
+//        Convert input  to decimal
         int decimalA = convertBinaryToDecimal(binaryA);
         int decimalB = convertBinaryToDecimal(binaryB);
-        int sum = decimalA + decimalB;
-        return Integer.parseInt(Integer.toString(sum), 2);
+//        Execute operation
+        int result = decimalA + decimalB;
+//        Convert to binary and return
+        return Integer.parseInt(Integer.toBinaryString(result));
     }
 
-    public int binarySubtraction(int binaryA, int binaryB){
+    public int binarySubtraction(int binaryA, int binaryB) {
+//        Convert input  to decimal
         int decimalA = convertBinaryToDecimal(binaryA);
         int decimalB = convertBinaryToDecimal(binaryB);
+//        Execute operation
         int result = decimalA - decimalB;
-        return Integer.parseInt(Integer.toString(result), 2);
+//        Convert to binary and return
+        return Integer.parseInt(Integer.toBinaryString(result));
     }
 
-    public int binaryMultiplication(int binaryA, int binaryB){
+    public int binaryMultiplication(int binaryA, int binaryB) {
+//        Convert input  to decimal
         int decimalA = convertBinaryToDecimal(binaryA);
         int decimalB = convertBinaryToDecimal(binaryB);
+//        Execute operation
         int result = decimalA * decimalB;
-        return Integer.parseInt(Integer.toString(result), 2);
+//        Convert to binary and return
+        return Integer.parseInt(Integer.toBinaryString(result));
     }
 
-    public int binaryDivision(int binaryA, int binaryB){
+    public int binaryDivision(int binaryA, int binaryB) {
+//        Convert input  to decimal
         int decimalA = convertBinaryToDecimal(binaryA);
         int decimalB = convertBinaryToDecimal(binaryB);
+//        Execute operation
         int result = decimalA / decimalB;
-        return Integer.parseInt(Integer.toString(result), 2);
+//        Convert to binary and return
+        return Integer.parseInt(Integer.toBinaryString(result));
     }
 }
