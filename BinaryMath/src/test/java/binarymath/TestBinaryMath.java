24203a84e7766c48d7e16b0712586749f7e74ae0
Alessandro Lioi <lioialessandro@users.noreply.github.com>
2022-02-17 10:40:44 +0100

    Close #2

---
+++ BinaryMath/src/test/java/binarymath/TestBinaryMath.java
@@ -0,0 +1,35 @@
+package binarymath;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBinaryMath {
+    @Test
+    public void testBinarySum() {
+        BinaryMath binaryMath = new BinaryMath();
+        int result = binaryMath.binarySum(0b01110, 0b1001);
+        assertEquals(10111, result);
+    }
+
+    @Test
+    public void testBinarySubtraction() {
+        BinaryMath binaryMath = new BinaryMath();
+        int result = binaryMath.binarySubtraction(0b01110, 0b1001);
+        assertEquals(101, result);
+    }
+
+    @Test
+    public void testBinaryMultiplication() {
+        BinaryMath binaryMath = new BinaryMath();
+        int result = binaryMath.binaryMultiplication(0b01110, 0b1001);
+        assertEquals(1111110, result);
+    }
+
+    @Test
+    public void testBinaryDivision() {
+        BinaryMath binaryMath = new BinaryMath();
+        int result = binaryMath.binaryDivision(0b01110, 0b1001);
+        assertEquals(1, result);
+    }
+}
